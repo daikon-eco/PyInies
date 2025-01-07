0711333c298731d331e8f294cf277d0f1a44f6c6
Alexandre Duverger <42521043+aduverger@users.noreply.github.com>
2025-01-07 12:38:31 +0100

    add logs in text file

--- pyinies/script.py
+++ pyinies/script.py
@@ -8,8 +8,27 @@ from pyinies.process import *
 from pyinies.client import IniesClient
 
 
+# Determine the base path
+if getattr(sys, "frozen", False):
+    # If the application is run as a bundle, the path is the executable directory
+    base_path = os.path.dirname(sys.executable)
+else:
+    # If run as a script, use the script's directory
+    base_path = os.path.dirname(os.path.abspath(__file__))
+
+# Create data and logs directories if they don't exist
+data_directory = os.path.join(base_path, "data")
+logs_directory = os.path.join(base_path, "logs")
+os.makedirs(data_directory, exist_ok=True)
+os.makedirs(logs_directory, exist_ok=True)
+
+current_time = datetime.now().strftime("%Y-%m-%d")
+log_file = os.path.join(logs_directory, f"{current_time}-logs.txt")
+
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s"
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[logging.FileHandler(log_file), logging.StreamHandler(sys.stdout)],
 )
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
@@ -29,7 +48,7 @@ def get_client():
 async def fetch_all_epds():
     client = get_client()
     try:
-        return await client.get_all_epds(since_date=datetime(2024, 12, 15))
+        return await client.get_all_epds()
     finally:
         await client.close()
 
@@ -55,19 +74,6 @@ def main():
     logging.info("Processing EPDs")
     df = process_all_epds(all_epds)
     df = modify_columns_names(df)
-
-    current_time = datetime.now().strftime("%Y-%m-%d")
-
-    # Determine the base path
-    if getattr(sys, "frozen", False):
-        # If the application is run as a bundle, the path is the executable directory
-        base_path = os.path.dirname(sys.executable)
-    else:
-        # If run as a script, use the script's directory
-        base_path = os.path.dirname(os.path.abspath(__file__))
-
-    data_directory = os.path.join(base_path, "data")
-    os.makedirs(data_directory, exist_ok=True)
 
     filename = os.path.join(data_directory, f"{current_time}-export_inies.xlsx")
 
